c265d578772ad1d78d4818941f65a96ea080683a
mooncyli-burise <mooncyli@bu.edu>
2026-07-23 16:23:06 -0400

    changed to homography

---
+++ backbone_model/real_world_objects.py
@@ -0,0 +1,50 @@
+import torch
+from backbone_model.simple_model_modified.dataset import Dataset
+import os
+from config import TEST_SIZE
+import json
+from simplified_dataset.simple_model.transforms import get_transforms
+from april_tags.get_data import get_apriltag_images
+from april_tags.create_ground_truth import create_ground_truth
+
+#use accelerator (offloading operations to gpu) or cpu if accelerator not available
+# device = torch.accelerator.current_accelerator() if torch.accelerator.is_available() else torch.device('cpu')
+
+device = torch.device('cpu')
+
+#set up ground truth data for training and testing
+ground_truth = create_ground_truth(get_apriltag_images('backbone_model/real_world_dataset'))
+
+# GridNet uses fixed-size images and scalar class targets, so the default
+# DataLoader collation produces image batches [B, C, H, W] and targets [B].
+dataset = Dataset('backbone_model/real_world_dataset/images', ground_truth, get_transforms())
+dataset_test = Dataset('backbone_model/real_world_dataset/images', ground_truth, get_transforms())
+
+#make list of same size as dataset and randomize order
+indices = torch.randperm(len(dataset)).tolist()
+dataset = torch.utils.data.Subset(dataset, indices[:-TEST_SIZE]) 
+#assign subset of last 50 of list for test
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-TEST_SIZE:])
+
+# dataset = torch.utils.data.Subset(dataset, indices[:50])
+# dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+# # dataset for overfitting below
+# dataset = torch.utils.data.Subset(dataset, indices[:50])
+# #assign subset of last 50 of list for test
+# dataset_test = torch.utils.data.Subset(dataset_test, indices[:50])
+
+
+#load train and test data
+data_loader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=32,
+    shuffle=True, 
+)
+
+data_loader_test = torch.utils.data.DataLoader(
+    dataset_test,
+    batch_size=32,
+    shuffle=False,
+)
+
--- main.py
+++ main.py
@@ -4,15 +4,38 @@ from backbone_model.simple_model_train_modified import train_simple
 from backbone_model.synthetic_limo_dataset.generate_synthetic_data import generate_synthetic_dataset
 # from synthetic_limo_testing.limo_train import train_limo
 from util.visualize_simple import visualize
-from april_tags.test_world_transformations import test_world_transformations
+from april_tags.test_world_transformations import test_world_transformations, test_multiple_tags
 from util.detect import detect_predict
 
+import cv2
+from april_tags.world_frame_transformations import get_homography, show_homography_view
+
 def main():
-    # test_world_transformations()
+    # cap = cv2.VideoCapture(0)
+
+    # H = get_homography()
+
+    # while True:
+
+    #     ret, frame = cap.read()
+
+    #     if not ret:
+    #         break
+
+    #     show_homography_view(frame, H)
+
+    #     if cv2.waitKey(1) == ord('q'):
+    #         break
+
+    # cap.release()
+    # cv2.destroyAllWindows()
+    
+    test_world_transformations()
+    # test_multiple_tags()
     # generate_synthetic_dataset(5000)
     # train_simple()
     # visualize('backbone_model/best_model_5000imgs.pth')
-    detect_predict('backbone_model/best_model_5000imgs.pth')
+    # detect_predict('backbone_model/best_model_5000imgs.pth')
 
     print("done!")
 
