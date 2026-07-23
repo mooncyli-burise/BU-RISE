9dce9df7c242fdc544d372c174218dfd4ef1ee01
mooncyli-burise <mooncyli@bu.edu>
2026-07-22 21:23:27 -0400

    tranformations

--- april_tags/world_frame_transformations.py
+++ april_tags/world_frame_transformations.py
@@ -73,6 +73,7 @@ def get_world_coords(pose):
     #                  [z]])
     R, T = get_rotation_and_translation_matrix()
     R_inverse = np.linalg.inv(R)
+    print("R:", R)
     print("R inverse:", R_inverse)
     T_z_only = np.array([[0],
                          [0],
@@ -81,6 +82,8 @@ def get_world_coords(pose):
     rotated_ray = np.dot(R_inverse, pose)
     multiplied_ray = rotated_ray * (-1*T[2][0]/rotated_ray[2][0])
     new_pose = T_z_only + multiplied_ray
+    print("Camera Z axis:")
+    print(R_inverse[:,2])
     print_transformation(R_inverse, T_z_only, pose, new_pose)
     return new_pose
     # return new_pose[0][0], new_pose[0][1], new_pose[0][2] # return x, y, and z separately
